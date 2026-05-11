508498d55b5ee6ff5a21681581d36a9049335892
Winter <2936545253@qq.com>
2026-05-11 15:19:51 +0800

    fix:github CI任务修复

--- backend/src/test/java/com/winter/airesumeoptimizer/ServeApplicationTests.java
+++ backend/src/test/java/com/winter/airesumeoptimizer/ServeApplicationTests.java
@@ -2,8 +2,10 @@ package com.winter.airesumeoptimizer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ServeApplicationTests {
 
     @Test
